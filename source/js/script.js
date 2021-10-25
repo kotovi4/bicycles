2107b2aa37630ad2e34c0b39398f2d71751f837d
kotovi4 <kotovi4.m@gmail.com>
2021-10-25 17:18:39 +0300

    добавляет закрытие меню и переход к блоку  по тапу на якорь

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,13 @@
 'use strict';
 
-//меню tablet и mobile
+//Меню tablet и mobile
 const pageHeader = document.querySelector('.page-header');
 const headerToggle = document.querySelector('.page-header__toggle');
+const mainNavList = document.querySelector('.main-nav-list');
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('#name');
+const telInput = document.querySelector('#tel');
+
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -15,3 +20,25 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.remove('page-header--opened');
   }
 });
+
+// Закрытие мобильного меню и скролл до якоря
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (const smoothLink of smoothLinks) {
+  if (smoothLink) {
+    smoothLink.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const id = smoothLink.getAttribute('href');
+
+      const placeScroll = document.querySelector(id);
+
+      if (placeScroll) {
+        placeScroll.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        pageHeader.classList.remove('page-header--opened');
+        pageHeader.classList.add('page-header--closed');
+      }
+    });
+  }
+}
