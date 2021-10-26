e31b10a930a180972ec923a96c8c4087d1dca48d
kotovi4 <kotovi4.m@gmail.com>
2021-10-26 12:04:01 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+//Меню tablet и mobile
+const pageHeader = document.querySelector('.page-header');
+const headerToggle = document.querySelector('.page-header__toggle');
+const mainNavList = document.querySelector('.main-nav-list');
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('#name');
+const telInput = document.querySelector('#tel');
+const formButton = form.querySelector('.form__button')
+const modal = document.querySelector('.modal');
+const modalClose = modal.querySelector('.modal__close');
+
+
+pageHeader.classList.remove('page-header--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+  }
+});
+
+// Закрытие мобильного меню и скролл до якоря
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (const smoothLink of smoothLinks) {
+  if (smoothLink) {
+    smoothLink.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const id = smoothLink.getAttribute('href');
+
+      const placeScroll = document.querySelector(id);
+
+      if (placeScroll) {
+        placeScroll.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        pageHeader.classList.remove('page-header--opened');
+        pageHeader.classList.add('page-header--closed');
+      }
+    });
+  }
+}
+
+//Отправка и валидация формы
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  //Валидация полей формы
+  if (nameInput && telInput) {
+    nameInput.addEventListener('input', function () {
+      if (nameInput.validity.patternMismatch) {
+        nameInput.setCustomValidity('Введите имя русскими буквами');
+      } else {
+        nameInput.setCustomValidity('');
+      }
+    });
+
+    telInput.addEventListener('input', function () {
+      if (telInput.validity.patternMismatch) {
+        telInput.setCustomValidity('Введите телефон в формате +7 ХХХ ХХХ ХХ ХХ');
+      } else {
+        telInput.setCustomValidity('');
+      }
+    });
+
+    formButton.addEventListener('click', function () {
+      localStorage.setItem(nameInput.name, nameInput.value);
+      localStorage.setItem(telInput.name, telInput.value);
+      modal.classList.add('modal--show');
+    });
+  }
+});
+
+//Закрыть модальное окно и сбросить инпуты
+modalClose.onclick = () => {
+  modal.classList.remove('modal--show');
+  form.reset();
+  telInput.reset();
+  nameInput.reset();
+}
